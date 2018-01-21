50a9b98b0462f792cdaa81331f296d9bdd4d05e4
Mason-Xu <xulihao1997@gmail.com>
2018-01-21 11:30:15 +0800

    add 带头结点的链式表的删除

--- 数据结构/中国MOOC/代码/3.2.3线性表的链式存储.cpp
+++ 数据结构/中国MOOC/代码/3.2.3线性表的链式存储.cpp
@@ -120,3 +120,28 @@ bool Insert(List L, ElementType X, int i)
         return true;
     }
 }
+
+// 带头结点的链式表的删除
+bool Delete(List L, int i)
+{   // 这里默认L有头结点
+    Position tmp, pre;
+    int cnt = 0;
+
+    // 查找位序为i-1的结点
+    pre = L;    // pre 指向表头
+    while(pre && cnt < i-1){
+        pre = pre->Next;
+        cnt++;
+    }
+    if(pre == NULL || cnt != i-1 || pre->Next == NULL){
+        // 所找结点或位序为i的结点不在L中
+        printf("插入位置参数错误!");
+        return false;
+    }else{  // 找到待删结点的前一个结点pre
+        // 将结点删除
+        tmp = pre->Next;
+        pre->Next = tmp->Next;
+        free(tmp);
+        return true;
+    }
+}
