999e734191b6022a5d70918690d2f0817a803d3d
Mason-Xu <xulihao1997@gmail.com>
2018-01-21 17:57:11 +0800

    fix Delete To DeleteQ

--- 数据结构/中国MOOC/代码/3.4.1 队列的定义.cpp
+++ 数据结构/中国MOOC/代码/3.4.1 队列的定义.cpp
@@ -6,4 +6,4 @@
 2. bool IsFull(Queue Q);    // 判断队列Q是否已满
 3. bool AddQ(Queue Q, ElementType X);   // 将元素X压入队列Q.队列已满,返回false,否则将数据元素X插入队列Q并返回true
 4. bool IsEmpty(Queue Q);   // 判断队列Q是否为空,若是,返回true,否则false
-5. ElementType Delete(Queue Q);     // 删除并返回队列头元素.若队列为空,返回错误信息,否则将队列头数据元素从队列删除并返回
+5. ElementType DeleteQ(Queue Q);     // 删除并返回队列头元素.若队列为空,返回错误信息,否则将队列头数据元素从队列删除并返回
